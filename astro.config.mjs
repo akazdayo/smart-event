2af802dee2489ca618c29f08ff9647357959bfe7
akazdayo <82073147+akazdayo@users.noreply.github.com>
2025-01-26 10:16:42 +0900

    cloudflare pagesにデプロイする

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,10 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import solidJs from '@astrojs/solid-js';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   output: 'server',
-  integrations: [tailwind(), solidJs()]
+  integrations: [tailwind(), solidJs()],
+  adapter: cloudflare()
 });
